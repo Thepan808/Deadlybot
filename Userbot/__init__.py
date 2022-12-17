2cd16c124b33134187e7c7daec51f402bf26354b
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-17 11:36:06 +0530

    Delete Userbot directory

--- Userbot/__init__.py
+++
@@ -1,5 +0,0 @@
-deadly_version = "1.0.0"
-source_version = "1.0"
-python_version = pytv
-pyrogram_version = ptv
-
--- Userbot/plugins/alive.py
+++
@@ -1,3 +0,0 @@
-   alive_text = "Hey Am Alive 🔥\n"
-   Alive_text += "Am Here to Help You With master\n"
-   
