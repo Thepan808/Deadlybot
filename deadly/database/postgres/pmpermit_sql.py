c0ea44d17d62603980cfa9eb341a321c815dd574
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-21 09:30:13 +0530

    Update pmpermit_sql.py

--- deadly/database/postgres/pmpermit_sql.py
+++ deadly/database/postgres/pmpermit_sql.py
@@ -1,146 +1,35 @@
-import threading
-
-from sqlalchemy import Column, Integer, String
-
-from . import BASE, SESSION
+from .import BASE, SESSION
+from sqlalchemy import Column, String
 
 
-# save user ids in whitelists
-class PMTABLE(BASE):
-    __tablename__ = "approve"
+class PMPermit(BASE):
+    __tablename__ = "pmpermit"
+    chat_id = Column(String(14), primary_key=True)
 
-    user_id = Column(Integer, primary_key=True)
-    boolvalue = Column(String)
-
-    def __init__(self, user_id, boolvalue):
-        self.user_id = user_id
-        self.boolvalue = boolvalue
+    def __init__(self, chat_id):
+        self.chat_id = str(chat_id)  # ensure string
 
 
-# save warn msg ids
-class MSGID(BASE):
-    __tablename__ = "pm msg id"
-
-    user_id = Column(Integer, primary_key=True)
-    msg_id = Column(Integer)
-
-    def __init__(self, user_id, msg_id):
-        self.user_id = user_id
-        self.msg_id = msg_id
+PMPermit.__table__.create(checkfirst=True)
 
 
-# save warn counts
-class DISAPPROVE(BASE):
-    __tablename__ = "disapprove"
-
-    user_id = Column(Integer, primary_key=True)
-    warn_count = Column(Integer)
-
-    def __init__(self, user_id, warn_count):
-        self.user_id = user_id
-        self.warn_count = warn_count
-
-
-PMTABLE.__table__.create(checkfirst=True)
-MSGID.__table__.create(checkfirst=True)
-DISAPPROVE.__table__.create(checkfirst=True)
-
-INSERTION_LOCK = threading.RLock()
-
-
-class PMPERMITSQL(object):
-    # add message id of a user
-    def set_msgid(self, user_id, msg_id):
-        with INSERTION_LOCK:
-            try:
-                user = SESSION.query(MSGID).get(user_id)
-                if not user:
-                    user = MSGID(user_id, msg_id)
-                else:
-                    user.msg_id = msg_id
-                SESSION.merge(user)
-                SESSION.commit()
-            finally:
-                SESSION.close()
-
-    # get warn message id
-    def get_msgid(self, user_id):
-        try:
-            user = SESSION.query(MSGID).get(user_id)
-            msg_id = None
-            if user:
-                msg_id = user.msg_id
-                return msg_id
-        finally:
-            SESSION.close()
-
-    # add user id to whitelist
-    def set_whitelist(self, user_id, boolvalue):
-        with INSERTION_LOCK:
-            user = SESSION.query(PMTABLE).get(user_id)
-            try:
-                if not user:
-                    user = PMTABLE(user_id, boolvalue)
-                else:
-                    user.boolvalue = str(boolvalue)
-                SESSION.add(user)
-                SESSION.commit()
-            finally:
-                SESSION.close()
-        return user_id
-
-    # remove user id from whitelist
-    def del_whitelist(self, user_id):
-        with INSERTION_LOCK:
-            user = SESSION.query(PMTABLE).get(user_id)
-            try:
-                if user:
-                    SESSION.delete(user)
-                    SESSION.commit()
-            finally:
-                SESSION.close()
-            return False
-
-    # get whitelist (approved)
-    def get_whitelist(self, user_id):
-        user = SESSION.query(PMTABLE).get(user_id)
-        rep = ""
-        if user:
-            rep = str(user.boolvalue)
+def is_approved(chat_id):
+    try:
+        return SESSION.query(PMPermit).filter(PMPermit.chat_id == str(chat_id)).one()
+    except BaseException:
+        return None
+    finally:
         SESSION.close()
-        return rep
 
-    # warn table func
-    def set_warn(self, user_id, warn_count):
-        with INSERTION_LOCK:
-            try:
-                user = SESSION.query(DISAPPROVE).get(user_id)
-                if not user:
-                    user = DISAPPROVE(user_id, warn_count)
-                else:
-                    user.warn_count = warn_count
-                SESSION.merge(user)
-                SESSION.commit()
-            finally:
-                SESSION.close()
 
-    # get warn func
-    def get_warn(self, user_id):
-        user = SESSION.query(DISAPPROVE).get(user_id)
-        rep = ""
-        if user:
-            rep = str(user.warn_count)
-        SESSION.close()
-        return rep
+def approve(chat_id):
+    adder = PMPermit(str(chat_id))
+    SESSION.add(adder)
+    SESSION.commit()
 
-    # del warn func
-    def del_warn(self, user_id):
-        with INSERTION_LOCK:
-            user = SESSION.query(DISAPPROVE).get(user_id)
-            try:
-                if user:
-                    SESSION.delete(user)
-                    SESSION.commit()
-            finally:
-                SESSION.close()
-            return False
+
+def dissprove(chat_id):
+    rem = SESSION.query(PMPermit).get(str(chat_id))
+    if rem:
+        SESSION.delete(rem)
+        SESSION.commit()
