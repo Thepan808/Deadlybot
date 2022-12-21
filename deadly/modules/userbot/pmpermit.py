d3bf4e427982ca64db071c4e477be018ad9bb775
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-21 09:59:56 +0530

    Update pmpermit.py

--- deadly/modules/userbot/pmpermit.py
+++ deadly/modules/userbot/pmpermit.py
@@ -1,291 +1,204 @@
-""" A security system which keeps spammers away """
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/Lunatic0de
 
-from pyrogram import filters
-from pyrogram.enums import ChatType
+
+from pyrogram import Client, enums, filters
 from pyrogram.types import Message
-from pyrogram.errors import (
-    PeerIdInvalid,
-    UsernameNotOccupied,
-    UsernameInvalid
+from sqlalchemy.exc import IntegrityError
+
+
+from deadly import TEMP_SETTINGS
+from deadly.core.basic import edit_or_reply
+from deadly.core.tools import get_arg
+
+
+PM_PIC = "https://te.legra.ph/file/7d2fa9c3ffaf22b846f6b.jpg"
+DEF_UNAPPROVED_MSG = (
+    "╔════════════════════╗\n"
+    "  ⛑ 𝗔𝗧𝗧𝗘𝗡𝗧𝗜𝗢𝗡 𝗣𝗟𝗘𝗔𝗦𝗘 ⛑  \n"
+    "╚════════════════════╝\n"
+    "• I haven't approved you for PM.\n"
+    "• Wait until I approve your PM.\n"
+    "• Don't Spam in Chat or you will be automatically banned.\n"
+    "╔════════════════════╗\n"
+    " 𝗣𝗼𝘄𝗲𝗿𝗲𝗱 𝗕𝘆 - [𝗗𝗲𝗮𝗱𝗹𝘆𝗨𝘀𝗲𝗿𝗯𝗼𝘁](https://t.me/DeadlyUserbot)  \n"
+    "╚════════════════════╝\n"
 )
 
-from deadly import app, gen
 
-
-
-
-app.CMD_HELP.update(
-    {"pmpermit" : (
-        "pmpermit",
-        {
-        "a" : "approve a user when pmpermit is on",
-        "da" : "disapprove a user when pmpermit is on"
-        }
-        )
-    }
+@app.on_message(
+    ~filters.me & filters.private & ~filters.bot & filters.incoming, group=69
 )
+async def incomingpm(client: Client, message: Message):
+    try:       
+        from deadly.database.postgres.pmpermit_sql import is_approved
+    except BaseException:
+        pass
+
+    if await auto_accept(client, message) or message.from_user.is_self:
+        message.continue_propagation()
+    if message.chat.id != 777000:
+        PM_LIMIT = 5
+        getmsg = DEF_UNAPPROVED_MSG
+        if getmsg is not None:
+            UNAPPROVED_MSG = getmsg
+        else:
+            UNAPPROVED_MSG = DEF_UNAPPROVED_MSG
+
+        apprv = is_approved(message.chat.id)
+        if not apprv and message.text != UNAPPROVED_MSG:
+            if message.chat.id in TEMP_SETTINGS["PM_LAST_MSG"]:
+                prevmsg = TEMP_SETTINGS["PM_LAST_MSG"][message.chat.id]
+                if message.text != prevmsg:
+                    async for message in client.search_messages(
+                        message.chat.id,
+                        from_user="me",
+                        limit=10,
+                        query=UNAPPROVED_MSG,
+                    ):
+                        await message.delete()
+                    if TEMP_SETTINGS["PM_COUNT"][message.chat.id] < (int(PM_LIMIT) - 1):
+                        ret = await message.reply_text(UNAPPROVED_MSG)
+                        TEMP_SETTINGS["PM_LAST_MSG"][message.chat.id] = ret.text
+            else:
+                ret = await message.reply_photo(photo=PM_PIC, caption=UNAPPROVED_MSG)
+                if ret.text:
+                    TEMP_SETTINGS["PM_LAST_MSG"][message.chat.id] = ret.text
+            if message.chat.id not in TEMP_SETTINGS["PM_COUNT"]:
+                TEMP_SETTINGS["PM_COUNT"][message.chat.id] = 1
+            else:
+                TEMP_SETTINGS["PM_COUNT"][message.chat.id] = (
+                    TEMP_SETTINGS["PM_COUNT"][message.chat.id] + 1
+                )
+            if TEMP_SETTINGS["PM_COUNT"][message.chat.id] > (int(PM_LIMIT) - 1):
+                await message.reply("**Sorry you have been blocked because of spamming in chat**")
+                try:
+                    del TEMP_SETTINGS["PM_COUNT"][message.chat.id]
+                    del TEMP_SETTINGS["PM_LAST_MSG"][message.chat.id]
+                except BaseException:
+                    pass
+
+                await client.block_user(message.chat.id)
+
+    message.continue_propagation()
 
 
+async def auto_accept(client, message):
+    try:
+        from deadly.database.postgres.pmpermit_sql import approve, is_approved
+    except BaseException:
+        pass
 
+    if message.chat.id in BLAZE:
+        try:
+            approve(message.chat.id)
+            await client.send_message(
+                message.chat.id,
+                f"<b>Receiving Messages!!!</b>\n{message.from_user.mention} <b>DeadlyUserbot Developer detected </b>",
+                parse_mode=enums.ParseMode.HTML,
+            )
+        except IntegrityError:
+            pass
+    if message.chat.id not in [client.me.id, 777000]:
+        if is_approved(message.chat.id):
+            return True
 
-async def old_msg(m: Message, user_id):
-    """ Delete warn messages """
-    if bool(app.get_msgid(user_id)) is True:
-        old_msgs = app.get_msgid(user_id)
-        await app.delete_messages(
-            chat_id=m.chat.id,
-            message_ids=old_msgs
-        )
-        return True
+        async for msg in client.get_chat_history(message.chat.id, limit=1):
+            if msg.from_user.id == client.me.id:
+                try:
+                    del TEMP_SETTINGS["PM_COUNT"][message.chat.id]
+                    del TEMP_SETTINGS["PM_LAST_MSG"][message.chat.id]
+                except BaseException:
+                    pass
+
+                try:
+                    approve(chat.id)
+                    async for message in client.search_messages(
+                        message.chat.id,
+                        from_user="me",
+                        limit=10,
+                        query=UNAPPROVED_MSG,
+                    ):
+                        await message.delete()
+                    return True
+                except BaseException:
+                    pass
+
     return False
 
 
-
-
-async def send_warn(m: Message, user):
-    """ Send warning messages """
-    pic = app.PmpermitPic()
-    text = app.PmpermitText()
-
-    if pic:
-        msg = await app.send_photo(
-            m.chat.id,
-            pic,
-            caption=text
-        )
-        app.set_msgid(user, msg.id)
-        return True
-    elif not pic:
-        msg = await app.send_message(
-            m.chat.id,
-            text,
-            disable_web_page_preview=True
-            )
-        app.set_msgid(user, msg.id)
-        return True
-    else:
-        return print("The bot didn't send pmpermit warning message.")
-
-
-
-
-
-
-# incoming autoblock
-@app.on_message(filters.private & filters.incoming & (~filters.bot & ~filters.me), group=-1)
-async def pmpermit_handler(_, m: Message):
-    """ pmpermit handler for pmpermit plugin """
-    try:
-        users = []
-        is_user = False
-        pmlimit = app.PmpermitLimit()
-
-        if bool(app.Pmpermit()) is False or m.chat.is_verified: # exclude verified accounts
-            return
-
-        if bool(app.get_whitelist(m.chat.id)) is True:
-            return
-        else:
-            # this will reduce the use of pyrogram's get_users method
-            if bool(users):
-                for x in users:
-                    if x.get(m.chat.id):
-                        is_user = True
-                        break
-
-            if is_user:
-                user = users.get(m.chat.id)
-            else:
-                user = await app.get_users(m.chat.id)
-                users.append({m.chat.id : user}) # whole object
-
-
-        # log user info to log chat
-
-        msg = "#pmpermit\n\n"
-        msg += f"Name: `{user.first_name}`\n"
-        msg += f"Id: `{user.id}`\n"
-        msg += f"Username: `@{user.username}`\n" if user.username else "Username: `None`\n"
-        msg += f"Message: `{m.text or m.caption}`\n"
-
-        warns = bool(app.get_warn(user.id))
-
-        if warns is False:
-            app.set_warn(user.id, 1)
-            await send_warn(m, user.id)
-
-        elif warns is True:
-            warn = int(app.get_warn(user.id))
-            if warn < pmlimit:
-                new_warn = warn + 1
-                app.set_warn(user.id, new_warn)
-                await old_msg(m, user.id) # delete old warns
-                await send_warn(m, user.id) # send new warns
-            elif warn >= pmlimit:
-                done = await app.block_user(user.id)
-                if done:
-                    try:
-                        await app.send_message(
-                            app.LOG_CHAT,
-                            f"{user.mention} is now blocked for spamming !"
-                        )
-                    except PeerIdInvalid:
-                        print(f"{user.first_name} was blocked in your pm for some reason.")
-                else:
-                    await app.send_edit(
-                        f"Failed to block {user.mention} because through pmpermit.",
-                        text_type=["mono"],
-                        delme=4
-                    )
-            else:
-                print("Something went wrong in pmpermit")
-    except Exception as e:
-        await app.error(e)
-
-
-
-
 @app.on_message(gen(["a", "approve"]), group=0)
-async def approve_handler(_, m: Message):
-    """ approve handler for pmpermit plugin """
-    if m.chat.type == ChatType.BOT:
-        return await app.send_edit(
-            "No need to approve innocent bots !",
-            text_type=["mono"],
-            delme=4
-        )
+async def approvepm(client: Client, message: Message):
+    try:
+        from deadly.database.postgres.pmpermit_sql  import approve
+    except BaseException:
+        await message.edit("Running on Non-SQL mode!")
+        return
 
-    reply = m.reply_to_message
-    cmd = m.command
-    user_data = False
-
-    if m.chat.type == ChatType.PRIVATE:
-        user_id = m.chat.id
-
-    elif m.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
-        if reply:
-            user_id = reply.from_user.id
-
-        elif not reply and app.long() == 1:
-            return await app.send_edit("Whom should i approve, piro ?", text_type=["mono"], delme=4)
-
-        elif not reply and app.long() > 1:
-            try:
-                user_data = await app.get_users(cmd[1])
-                user_id = user_data.id
-            except PeerIdInvalid:
-                return await app.send_edit(
-                    "You have to pass username instead of user id.",
-                    text_type=["mono"],
-                    delme=4
-                )
-            except UsernameNotOccupied:
-                return await app.send_edit(
-                    "This user doesn't exists in telegram.",
-                    text_type=["mono"],
-                    delme=4
-                )
-            except UsernameInvalid:
-                return await app.send_edit(
-                    "The username | user id is invalid.",
-                    text_type=["mono"],
-                    delme=4
-                )
-
-        else:
-            return await app.send_edit("Something went wrong.", text_type=["mono"], delme=4)
-    if user_data:
-        info = user_data
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        replied_user = reply.from_user
+        if replied_user.is_self:
+            await message.edit("You can't approve yourself.")
+            return
+        aname = replied_user.id
+        name0 = str(replied_user.first_name)
+        uid = replied_user.id
     else:
-        info = await app.get_users(user_id)
+        aname = message.chat
+        if not aname.type == enums.ChatType.PRIVATE:
+            await message.edit(
+                "You're not currently in PM and you haven't replied to someone's message."
+            )
+            return
+        name0 = aname.first_name
+        uid = aname.id
 
     try:
-        m = await app.send_edit(f"`Approving` {info.mention} `. . .`")
-        app.set_whitelist(user_id, True)
-        await app.send_edit(f"{info.mention} `is now approved.`", delme=4)
-        app.del_warn(user_id)
-
-        if app.get_msgid(user_id):
-            await old_msg(m, user_id)
-
-    except Exception as e:
-        await app.send_edit("Something went wrong.", text_type=["mono"], delme=4)
-        await app.error(e)
-
-
-
-
-@app.on_message(gen(["da", "disapprove"]), group=-2)
-async def diapprove_handler(_, m:Message):
-    """ disapprove handler for pmpermit plugin """
-    if m.chat.type == ChatType.BOT:
-        return await app.send_edit(
-            "No need to approve innocent bots !",
-            text_type=["mono"],
-            delme=4
+        approve(uid)
+        await message.edit(f"**Receive Messages From** [{name0}](tg://user?id={uid})!")
+    except IntegrityError:
+        await message.edit(
+            f"[{name0}](tg://user?id={uid}) probably already approved for PM."
         )
+        return
 
-    reply = m.reply_to_message
-    cmd = m.command
-    user_data = False
 
-    if m.chat.type == ChatType.PRIVATE:
-        user_id = m.chat.id
+@app.on_message(gen(["da", "disapprove"]), group=0)
+async def disapprovepm(client: Client, message: Message):
+    try:
+        from deadly.database.postgres.pmpermit_sql  import dissprove
+    except BaseException:
+        await message.edit("Running on Non-SQL mode!")
+        return
 
-    elif m.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
-        if reply:
-            user_id = reply.from_user.id
-
-        elif not reply and app.long() == 1:
-            return await app.send_edit(
-                "Whom should i disapprove, piro ?",
-                text_type=["mono"],
-                delme=4
-            )
-
-        elif not reply and app.long() > 1:
-            try:
-                user_data = await app.get_users(cmd[1])
-                user_id = user_data.id
-            except PeerIdInvalid:
-                return await app.send_edit(
-                    "Pass username instead of user id.",
-                    text_type=["mono"],
-                    delme=4
-                )
-            except UsernameNotOccupied:
-                return await app.send_edit(
-                    "This user doesn't exists in telegram.",
-                    text_type=["mono"],
-                    delme=4
-                )
-            except UsernameInvalid:
-                return await app.send_edit(
-                    "The username | user id is invalid.",
-                    text_type=["mono"],
-                    delme=4
-                )
-        else:
-            return await app.send_edit("Failed to disapprove user !", text_type=["mono"], delme=4)
-    if user_data:
-        info = user_data
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        replied_user = reply.from_user
+        if replied_user.is_self:
+            await message.edit("You can't deny yourself.")
+            return
+        aname = replied_user.id
+        name0 = str(replied_user.first_name)
+        uid = replied_user.id
     else:
-        info = await app.get_users(user_id)
-
-    if info:
-        await app.send_edit(f"`Disapproving` {info.mention} `. . .`")
-        app.del_whitelist(user_id)
-        await app.send_edit(f"{info.mention} `has been disapproved for pm.`", delme=4)
-        try:
-            await app.send_message(
-                app.LOG_CHAT,
-                f"#disapprove\n\n{info.mention} `has been disapproved.`"
+        aname = message.chat
+        if not aname.type == enums.ChatType.PRIVATE:
+            await message.edit(
+                "You're not currently in PM and you haven't replied to someone's message."
             )
-        except PeerIdInvalid:
-            print(f"{info.first_name} has been disapproved.")
-    else:
-        return await app.send_edit(
-            "Sorry there is no user id to disapprove.",
-            text_type=["mono"],
-            delme=4
-        )
+            return
+        name0 = aname.first_name
+        uid = aname.id
+
+    dissprove(uid)
+
+    await message.edit(
+        f"**Message** [{name0}](tg://user?id={uid}) **Rejected, Please Don't Spam Chat!**"
+    )
