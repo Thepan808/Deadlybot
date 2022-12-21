7d9572914b93dff9550a2e153948fe9fb31366cd
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-21 10:08:10 +0530

    Update __init__.py

--- deadly/database/__init__.py
+++ deadly/database/__init__.py
@@ -1,6 +1,6 @@
 from .postgres.afk_sql import AFKSQL
 from .postgres.notes_sql import NOTESSQL
-from .postgres.pmpermit_sql import PMPERMITSQL
+from .postgres.pmpermit_sql import PMPermit
 from .postgres.dv_sql import DVSQL
 from .postgres.welcome_sql import WELCOMESQL
 
@@ -9,7 +9,7 @@ from .postgres.welcome_sql import WELCOMESQL
 class Database(
     AFKSQL,
     NOTESSQL,
-    PMPERMITSQL,
+    PMPermit,
     DVSQL,
     WELCOMESQL
     ):
