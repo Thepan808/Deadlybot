74aba50f8e61ef8beb57d35aa98d31945a6866cf
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-21 13:30:29 +0530

    Update homepage.py

--- deadly/modules/assistant/callbacks/homepage.py
+++ deadly/modules/assistant/callbacks/homepage.py
@@ -22,11 +22,7 @@ async def _start(_, cb: CallbackQuery):
     await cb.edit_message_media(
         media=InputMediaPhoto(media=app.BotPic(), caption=app.home_tab_string()),
         reply_markup=InlineKeyboardMarkup([
-                app.BuildKeyboard(
-                            (                                
-                                ["๏ Plugins ๏", "plugins-tab"]
-                            )
-                        ),
+                app.BuildKeyboard(([["๏ Plugins ๏", "plugins-tab"]])),
                 app.BuildKeyboard(
                             (
                                 ["๏ Assistant ๏", "assistant-tab"], 
