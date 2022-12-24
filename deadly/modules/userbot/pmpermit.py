7883d0460e20c5767f1122c4c3814a489dabeac1
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-12-24 17:22:25 +0530

    Update pmpermit.py

--- deadly/modules/userbot/pmpermit.py
+++ deadly/modules/userbot/pmpermit.py
@@ -43,7 +43,7 @@ async def incomingpm(client: Client, message: Message):
     except BaseException:
         pass
 
-    if await auto_accept(client, message) or message.from_user.is_self:
+    if await auto_accept(client, message)
         message.continue_propagation()
     if message.chat.id != 777000:
         PM_LIMIT = 5
